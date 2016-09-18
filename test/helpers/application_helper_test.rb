026f768ecffb204e4a8611d5328ef896ff62e3ee
Erin McLaughlin <emclaughlin1215@gmail.com>
2016-09-18 12:17:06 -0400

    Removing the old files, as the entire project is starting over.

--- test/helpers/application_helper_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    assert_equal full_title, "Cat Farm App"
-    assert_equal full_title("Help"), "Help | Cat Farm App"
-  end
-end
